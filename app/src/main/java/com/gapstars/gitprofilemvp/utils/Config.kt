7fcab1c8ac10d9acf18eb63f16003d42192784dd
Mihira Prasanna <prasannamihira@gmail.com>
2021-04-27 19:45:57 +0530

    App configs

--- app/src/main/java/com/gapstars/gitprofilemvp/utils/Config.kt
+++ app/src/main/java/com/gapstars/gitprofilemvp/utils/Config.kt
@@ -3,14 +3,8 @@ package com.gapstars.gitprofilemvp.utils
 interface Config {
 
     companion object {
-        val TIME_OUT = 20
 
         const val CONTENT_TYPE_JSON = "Content-Type:application/json"
-        const val ACCESS_TOKEN_PREFIX = "Bearer"
         const val AUTHORIZATION = "Authorization"
-
-        const val RESPONSE_SUCCESS = 200
-        const val AUTHENTICATION_ERROR = "401"
-        const val NOT_FOUND = "404"
     }
 }
