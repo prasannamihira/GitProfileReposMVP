87e33f9a6afaf6cb0b0579b20acdfe9ab1c9640a
Mihira Prasanna <prasannamihira@gmail.com>
2021-05-01 12:10:14 +0530

    Cache management

--- app/src/main/java/com/gapstars/gitprofilemvp/injection/module/NetworkModule.kt
+++ app/src/main/java/com/gapstars/gitprofilemvp/injection/module/NetworkModule.kt
@@ -1,14 +1,20 @@
 package com.gapstars.gitprofilemvp.injection.module
 
+import android.content.Context
 import com.gapstars.gitprofilemvp.BuildConfig
+import com.gapstars.gitprofilemvp.network.GitProfileApi
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import io.reactivex.schedulers.Schedulers
-import com.gapstars.gitprofilemvp.network.GitProfileApi
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
+
 
 /**
  * Module which provides all required dependencies about network
@@ -36,11 +42,70 @@ object NetworkModule {
     @Provides
     @Reusable
     @JvmStatic
-    internal fun provideRetrofitInterface(): Retrofit {
+    internal fun provideRetrofitInterface(context: Context): Retrofit {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val httpCacheDirectory = File(context.cacheDir, "offlineCache")
+
+        //10 MB
+        val cache = Cache(httpCacheDirectory, 10 * 1024 * 1024)
+
+        val httpClient: OkHttpClient = OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor(httpLoggingInterceptor)
+            .addNetworkInterceptor(provideCacheInterceptor())
+            .addInterceptor(provideOfflineCacheInterceptor())
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
+//            .client(httpClient)
             .addConverterFactory(MoshiConverterFactory.create().withNullSerialization())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
             .build()
+    }
+
+    private fun provideCacheInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            var request: Request = chain.request()
+            val originalResponse: Response = chain.proceed(request)
+            val cacheControl: String? = originalResponse.header("Cache-Control")
+            if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains(
+                    "no-cache"
+                ) ||
+                cacheControl.contains("must-revalidate") || cacheControl.contains("max-stale=0")
+            ) {
+                val cacheControl: CacheControl = CacheControl.Builder()
+                    .maxStale(1, TimeUnit.DAYS) // cache for one day
+                    .build()
+                request = request.newBuilder()
+                    .removeHeader("Pragma")
+                    .cacheControl(cacheControl)
+                    .build()
+                chain.proceed(request)
+            } else {
+                originalResponse
+            }
+        }
+    }
+
+    private fun provideOfflineCacheInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            try {
+                chain.proceed(chain.request())
+            } catch (e: Exception) {
+                val cacheControl: CacheControl = CacheControl.Builder()
+                    .onlyIfCached()
+                    .maxStale(1, TimeUnit.DAYS) // cache for one day
+                    .build()
+                val offlineRequest: Request = chain.request().newBuilder()
+                    .removeHeader("Pragma")
+                    .header("Cache-Control", "public, only-if-cached")
+                    .cacheControl(cacheControl)
+                    .build()
+                chain.proceed(offlineRequest)
+            }
+        }
     }
 }
--- app/src/main/java/com/gapstars/gitprofilemvp/network/GitProfileApi.kt
+++ app/src/main/java/com/gapstars/gitprofilemvp/network/GitProfileApi.kt
@@ -4,15 +4,16 @@ import com.gapstars.githubprofilerepository.data.remote.data.request.QueryReques
 import com.gapstars.gitprofilemvp.model.data.response.GitProfileDataResponse
 import com.gapstars.gitprofilemvp.utils.Config
 import io.reactivex.Flowable
-import io.reactivex.Observable
 import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 /**
  * The interface which provides methods to get result of webservices
  */
 interface GitProfileApi {
-
 
     /**
      * query github profile data
